28c49d56d030c050123b6ebb3466df6803f97588
SomndusStudio <74624954+SomndusStudio@users.noreply.github.com>
2023-09-06 23:12:09 +0200

    Move and rename

---
+++ Source/ACC_ProjectExample/ACC_ProjectExample.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ACC_ProjectExample.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ACC_ProjectExample, "ACC_ProjectExample" );
